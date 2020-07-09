9634c21215519bdb790fdf1dcd148882cd30affa
ninjanaveen8888 <68016645+ninjanaveen8888@users.noreply.github.com>
2020-07-09 09:21:44 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -9,19 +9,19 @@ from userbot.utils import admin_cmd
 
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
-ALIVE_IMG = "https://telegra.ph/file/f580038f91382b976d5f8.jpg"
-ALIVE_caption = "`ARJUN USERBOT IS:` **ONLINE**\n\n"
+ALIVE_IMG = "https://telegra.ph/file/27932853966c10bede626.png"
+ALIVE_caption = "`NINJA USERBOT IS:` **ONLINE**\n\n"
 ALIVE_caption += "**SYSTEM STATUS**\n\n"
 ALIVE_caption += "`TELETHON VERSION:` **6.0.9**\n`Python:` **3.7.4**\n\n"
 ALIVE_caption += "`DATABASE STATUS:` **Functional**\n\n"
 ALIVE_caption += "**Current Branch** : `master`\n\n"
-ALIVE_caption += "**Friday OS** : `3.14`\n\n"
-ALIVE_caption += "**Current Sat** : `ArjunSat-2.25`\n\n"
-ALIVE_caption += f"**My Boss** : {DEFAULTUSER} \n\n"
+ALIVE_caption += "**Ninja OS** : `3.14`\n\n"
+ALIVE_caption += "**Current Sat** : `NinjaSat-2.25`\n\n"
+ALIVE_caption += f"**My Master** : {DEFAULTUSER} \n\n"
 ALIVE_caption += "**Heroku Database** : `AWS - Working Properly`\n\n"
-ALIVE_caption += "**License** : Mit License \n\n"
-ALIVE_caption += "Copyright By [Arjun@Telegram](t.me/IamArjun078)\n\n"
-ALIVE_caption += "[Deploy Arjun Bot](t.me/IamArjun078)"
+ALIVE_caption += "**Join the Bot Creator's Channel** : [💠Royal Giveaways💠](t.me/royal_giveaway)\n\n"
+ALIVE_caption += "USERBOT By [🔥Ninja Naveen🔥](t.me/NinjaNaveen)\n\n"
+ALIVE_caption += "[Deploy Ninja Bot](https://heroku.com/deploy?template=https://github.com/ninjanaveen/fridayuserbot))"
 #@command(outgoing=True, pattern="^.alive$")
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
